712c5b1ddb931e24c4208db16e488f4c7f65a0e7
Jani Lahti <jani.lahti@finnpos.fi>
2018-03-28 18:24:32 +0300

    #9 added more paper sizes (a5, a5Land, a3, a3Land, legal, legalLand)

--- src/extension.js
+++ src/extension.js
@@ -128,6 +128,14 @@ function buildHtml(text, language) {
 
   // these could be moved to package.json as configuration objects
   let paperSpecs = {
+    a3: {
+      name: "A3",
+      width: "297mm"
+    },
+    a3Land: {
+      name: "A3 landscape",
+      width: "420mm"
+    },
     a4: {
       name: "A4",
       width: "210mm"
@@ -136,13 +144,29 @@ function buildHtml(text, language) {
       name: "A4 landscape",
       width: "297mm"
     },
+    a5: {
+      name: "A5",
+      width: "148mm"
+    },
+    a5Land: {
+      name: "A5 landscape",
+      width: "210mm"
+    },
     letter: {
       name: "letter",
       width: "216mm"
     },
     letterLand: {
       name: "letter landscape",
-      width: "279mm"
+      width: "280mm"
+    },
+    legal: {
+      name: "legal",
+      width: "216mm"
+    },
+    legalLand: {
+      name: "legal landscape",
+      width: "357mm"
     }
   };
 
